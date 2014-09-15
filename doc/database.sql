edf8a0638d5b85d9356b1a7871803fb68e7251c2
Syret <kart.syret@khk.ee>
2014-09-15 15:32:47 +0300

    Add comment support

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Sept 08, 2014 kell 01:45 PL
+-- Loomise aeg: Sept 15, 2014 kell 02:30 PL
 -- Serveri versioon: 5.6.20
 -- PHP versioon: 5.5.15
 
@@ -18,6 +18,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabeli struktuur tabelile `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `author` varchar(25) NOT NULL,
+  `comment_text` text NOT NULL,
+  `comment_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Andmete tõmmistamine tabelile `comment`
+--
+
+INSERT INTO `comment` (`author`, `comment_text`, `comment_created`, `post_id`) VALUES
+  ('Syret', 'This post is awesome', '2014-09-15 07:45:12', 1),
+  ('', 'See on tõesti opaaaaa postitus', '2014-09-15 11:04:56', 5),
+  ('Tuuli Raudne', 'Ses suhtes, et eksole, onju ongi nii et, sedasi peakski olema.', '2014-09-15 12:25:38', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Tabeli struktuur tabelile `post`
 --
 
@@ -28,14 +51,61 @@ CREATE TABLE IF NOT EXISTS `post` (
   `post_text` text NOT NULL,
   `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Andmete tõmmistamine tabelile `post`
 --
 
 INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
-  (1, 'Esimene postitus', 'Kas see töötab? Loodame', '2014-09-04 13:08:28', 1);
+  (1, 'Esimene postitus', 'Kas see töötab? Loodame', '2014-09-04 13:08:28', 1),
+  (2, 'Teine postitus', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada augue et condimentum aliquam. Quisque tincidunt nunc mauris, accumsan vehicula arcu eleifend ut. Ut pulvinar enim et lectus eleifend placerat. In et magna sit amet tortor elementum hendrerit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum dignissim ligula urna, at interdum orci euismod a', '2014-09-08 13:02:44', 1),
+  (5, 'Kolmas', 'Opaaaaa', '2014-09-10 13:20:36', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `post_tags`
+--
+
+DROP TABLE IF EXISTS `post_tags`;
+CREATE TABLE IF NOT EXISTS `post_tags` (
+  `post_id` int(11) unsigned NOT NULL,
+  `tag_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Andmete tõmmistamine tabelile `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+  (1, 1),
+  (2, 1),
+  (1, 2),
+  (5, 2),
+  (1, 8),
+  (5, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE IF NOT EXISTS `tag` (
+  `tag_id` int(10) unsigned NOT NULL,
+  `tag_name` varchar(25) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Andmete tõmmistamine tabelile `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
+  (1, 'one'),
+  (2, 'Syret'),
+  (8, 'essa');
 
 -- --------------------------------------------------------
 
@@ -69,6 +139,18 @@ ALTER TABLE `post`
 ADD PRIMARY KEY (`post_id`), ADD KEY `user_id` (`user_id`);
 
 --
+-- Indeksid tabelile `post_tags`
+--
+ALTER TABLE `post_tags`
+ADD PRIMARY KEY (`post_id`,`tag_id`), ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indeksid tabelile `tag`
+--
+ALTER TABLE `tag`
+ADD PRIMARY KEY (`tag_id`);
+
+--
 -- Indeksid tabelile `user`
 --
 ALTER TABLE `user`
@@ -82,7 +164,12 @@ ADD PRIMARY KEY (`user_id`);
 -- AUTO_INCREMENT tabelile `post`
 --
 ALTER TABLE `post`
-MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT tabelile `tag`
+--
+ALTER TABLE `tag`
+MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT tabelile `user`
 --
@@ -97,4 +184,11 @@ MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 ALTER TABLE `post`
 ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Piirangud tabelile `post_tags`
+--
+ALTER TABLE `post_tags`
+ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
+ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
 SET FOREIGN_KEY_CHECKS=1;
